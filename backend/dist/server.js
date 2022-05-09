6ba33a8fc490ca860f33106bf13c46221b2aa3a3
Yago Russo <yagrrusso@gmail.com>
2022-05-09 00:46:46 -0300

    backend ajustes

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,14 +1,15 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-const teste = 1;
-const testeFn = () => __awaiter(void 0, void 0, void 0, function* () {
-    return 'ok';
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const routes_1 = require("./routes");
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(routes_1.routes);
+app.listen(3333, () => {
+    console.log('Server Running');
 });
